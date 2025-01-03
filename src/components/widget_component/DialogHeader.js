646f69a62a84c12b86d3a836986946251e07369f
amirbeek <amirbek.shomurodov01@gmail.com>
2025-01-03 21:04:09 +0000

    lkm

--- src/components/widget_component/DialogHeader.js
+++ src/components/widget_component/DialogHeader.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { DialogTitle, IconButton, SvgIcon } from '@mui/material';
 import { X } from "react-feather";
 
-// Reusable DialogHeader component
 const DialogHeader = ({ title, onClose }) => {
     return (
         <DialogTitle>
--- src/sections/MainSection.js
+++ src/sections/MainSection.js
@@ -4,7 +4,6 @@ import { Grid } from '@mui/material';
 import Bimage from '../background_images/Bimage.png';
 import { useNavigate } from 'react-router-dom';
 
-// Define the styled components outside the main component for better performance and structure
 const GoogleButtonStyle = styled.button`
     padding: 10px;
     font-weight: bolder;
